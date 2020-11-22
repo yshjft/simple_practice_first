4774d66497ddf911edfdaf9ef0e8fafcdea7a9b1
hong yuseok <jericoh@naver.com>
2020-11-23 00:55:45 +0900

    first commit

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module '*.css' {
+    const classes: {[key: string]: string}
+    export default classes
+  }
+  
+  declare module '*.scss' {
+    const classes: {[key: string]: string}
+    export default classes
+  }
