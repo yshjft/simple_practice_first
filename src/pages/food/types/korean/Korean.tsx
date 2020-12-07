211352f406218a7c4bb55aa130d8752da924d50c
hong yuseok <jericoh@naver.com>
2020-12-08 01:14:33 +0900

    making component....

--- src/pages/food/types/korean/Korean.tsx
+++ src/pages/food/types/korean/Korean.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import styles from './Korean.scss'
 
 const Korean = ()=>{
     return(
-        <div>korean foods</div>
+        <div>
+            <div className={`${styles.place}`}>
+                <div className={`${styles.contentLayout}`}>
+                    <div className={`${styles.imgArea}`}>사진</div>
+                    <div>
+                        <div>이름</div>
+                        <div>평점</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
