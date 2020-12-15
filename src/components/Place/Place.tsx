3fd7af7c44c10818baa05253ad8a65884b2ac720
hong yuseok <jericoh@naver.com>
2020-12-16 02:10:49 +0900

    useSelector 연결

--- src/components/Place/Place.tsx
+++ src/components/Place/Place.tsx
@@ -3,24 +3,26 @@ import styles from './Place.scss'
 import image from '../../Image/restaurant.jpeg'
 
 interface IProps {
+    name: string
+    score: number
     setModalVisible: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const Place = (props: IProps) => {
-    const {setModalVisible} = props
+    const {name, score, setModalVisible} = props
 
     return (
         <>
             <div onClick={()=>setModalVisible(true)} className={`${styles.place}`}>
                 <div className={`${styles.contentLayout}`}>
                     <div className={`${styles.imgArea}`}>
-                        {/* <div className={`${styles.noImg}`}>NO IMAGE</div> */}
-                        <img src={image} className={`${styles.Img}`}></img>
+                        <div className={`${styles.noImg}`}>NO IMAGE</div>
+                        {/* <img src={image} className={`${styles.Img}`}></img> */}
                     </div>
                     <div className={`${styles.infoArea}`}>
                         <div>
-                            <div className={`${styles.infoName}`}>이름</div>
-                            <div className={`${styles.infoScore}`}>평점</div>
+                            <div className={`${styles.infoName}`}>{name}</div>
+                            <div className={`${styles.infoScore}`}>{score} / 5</div>
                         </div>
                     </div>
                 </div>
--- src/pages/food/types/korean/Korean.tsx
+++ src/pages/food/types/korean/Korean.tsx
@@ -1,62 +1,26 @@
-import React, {useEffect, useState, useRef} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
+import {RootState} from '../../../../modules/index'
+import {FoodState} from '../../../../modules/food'
 import Place from '../../../../components/Place/Place'
 import Modal from '../../../../components/common/Modal/Modal'
 import styles from './Korean.scss'
-import {RootState} from '../../../../modules/index'
+
+
 
 const Korean = ()=>{
     const [modalVisible, setModalVisible] = useState<boolean>(false)
-    const food  = useSelector((state:RootState)=>state.food)
-    console.log(food)
+    const foods = useSelector((state:RootState)=>state.food)
+    const [koreanFoods, setKoreanFoods] = useState<FoodState[]>([])
+
+    useEffect(()=>{
+        setKoreanFoods(foods.filter(food => food.type === 'korean'))
+    }, [foods])
 
     return(
         <>
             <div className={`${styles.koreanLayout}`}>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
-                <Place setModalVisible={setModalVisible}/>
+                {koreanFoods.map((koreanFood, index) => <Place key={index} name={koreanFood.name} score={koreanFood.score} setModalVisible={setModalVisible}/>)}
             </div>
             <Modal modalVisible={modalVisible} setModalVisible={setModalVisible}/>
         </>
